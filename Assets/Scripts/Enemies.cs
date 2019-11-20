caeb071a529f29db26c49a453ee747e96c0e1b15
Ogan <ogan158@hotmail.com>
2019-11-20 17:31:14 +0300

    Add UI

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private int speed;
+    Rigidbody rb;
+    [SerializeField]
+    GameManager gm;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Castle")
+        {
+            gm.GameOver();
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject canvasMenu;
+
+    [SerializeField]
+    private GameObject canvasGameOver;
+
+    [SerializeField]
+    private GameObject canvasInGame;
+
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale =  1;
+        canvasMenu.SetActive(false);
+        canvasInGame.SetActive(true);
+
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale =  0;
+        canvasGameOver.SetActive(true);
+        canvasInGame.SetActive(false);
+    }
+
+    public void Replay()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        canvasMenu.SetActive(false);
+        canvasGameOver.SetActive(false);
+        canvasInGame.SetActive(true);
+    }
+}
